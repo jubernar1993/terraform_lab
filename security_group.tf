a3ae5d4e6e6e4d8aaa511788f3a559a80c5f9f7f
July Bernard <julybernard1993@gmail.com>
2023-05-04 17:10:39 +0000

    changes to the security_group file

--- security_group.tf
+++ security_group.tf
@@ -5,7 +5,11 @@ dynamic "ingress"{
     for_each = ["80","22","443"]
 
     content {
-        ingress 
+        ingress {
+from_port = value.ingress
+to_port = value_ingress 
+protocol = "tcp"
+cidr_block = ["0.0.0.0/0"] 
     }
 }
 
